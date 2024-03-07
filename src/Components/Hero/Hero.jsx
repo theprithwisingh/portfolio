d4d734a208d48f3c489b336ad150279942dd1f12
theprithwisingh <prithwisingh77@gmail.com>
2024-03-07 21:28:52 +0530

    4-change

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,13 +1,18 @@
-import './Hero.css'
-
+import './Hero.css';
+import icon from "../../assets/img.png"
 function Hero() {
   return (
-    <div className='Hero'>
-      <div className='card-1'>1</div>
-      <div className='card-1'>1</div>
-      <div className='card-1'>1</div>
+    <div className="hero-section">
+      <div className="hero-content">
+        <img src={icon} alt="" />
+        <h1>Hi, I'm Prithwi</h1>
+        <p>Building digital products, brands, and experiences.</p>
+        <p>A Product Designer and Visual Developer in SF.</p>
+        <p>I specialize in UI/UX Design, Responsive Web Design, and Visual Development.</p>
+        <button>CONNECT WITH ME</button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
