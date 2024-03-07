d00f8bcafc98a737e05de0c8d9711f4063c1bb11
theprithwisingh <prithwisingh77@gmail.com>
2024-03-07 19:04:21 +0530

    first portfolio commit-save

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Navbar from './Components/Navbar/Navbar'
+
+function App() {
+  return (
+    <div className='App'>
+      <Navbar/>
+      <div className="hero-section">
+        <div className=''></div>
+        <div className=''></div>
+        <div className=''></div>
+      </div>
+
+    </div>
+  )
+}
+
+export default App;
