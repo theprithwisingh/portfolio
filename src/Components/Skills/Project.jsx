ced2b4c14504bc9714ca968ba52162d6ac533ae4
theprithwisingh <prithwisingh77@gmail.com>
2024-05-07 23:51:37 +0530

    json

--- src/Components/Skills/Project.jsx
+++ src/Components/Skills/Project.jsx
@@ -4,14 +4,36 @@ import food from '../../assets/food.jpg'
 function Project() {
   return (
     <div className='projects'>
+      <div className='heading-line'>
+      <h1>These are the projects I have worked on so
+     </h1>
+     <p>I've Worked On Plenty Of Projects, But These Are The Ones I'm Really Proud Of. Many Are Open-Source, So If You Spot Something You Like, Check Out The Code And Feel Free To Pitch In With Your Ideas For Making It Even Better.</p>
+      </div>
       <div className='projectCards'>
         <div className='left'>
          <img src={food} alt="" className='poster-image' />
+         <div className='btn'>
          <button>Live Demo</button>
          <button>GitHub</button>
+         </div>
         </div>
         <div className='right'>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi eius est repellat laborum dicta non ducimus repellendus nulla, veritatis sed fugiat id? Quae nisi cumque illum ipsa vero repudiandae sed!snap
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et molestias enim ab ea! Ducimus ratione molestiae nisi accusamus culpa error, maxime laborum quia provident asperiores pariatur? Natus odio esse officia reprehenderit at omnis delectus nemo est, ducimus enim asperiores saepe animi minima deleniti suscipit numquam quibusdam excepturi et labore autem.
+          <span>...More detals</span>
+          </p>
+        </div>
+      </div>
+{/* 2 */}
+      <div className='projectCards'>
+        <div className='left'>
+         <img src={food} alt="" className='poster-image' />
+         <div className='btn'>
+         <button>Live Demo</button>
+         <button>GitHub</button>
+         </div>
+        </div>
+        <div className='right'>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et molestias enim ab ea! Ducimus ratione molestiae nisi accusamus culpa error, maxime laborum quia provident asperiores pariatur? Natus odio esse officia reprehenderit at omnis delectus nemo est, ducimus enim asperiores saepe animi minima deleniti suscipit numquam quibusdam excepturi et labore autem.
           <span>...More detals</span>
           </p>
         </div>
