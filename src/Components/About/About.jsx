88369aa5996eacfd934032b86d1fcee02da31244
theprithwisingh <prithwisingh77@gmail.com>
2024-07-26 22:33:00 +0530

    updates

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -13,13 +13,15 @@ const About = () => {
 
   return (
     <div className="about-section">
-      <div className='card'>
+      <div className='left'>
+
       <div className='pic-name-position'>
         <img src={profileImage} alt="" />
         <p>Prithwi singh</p>
         <p>Software engineer & Freelancer</p>
       </div>
-      <hr />
+
+      <hr/>
       <div className='email-location-socialmedia'>
         <div className='email'>
           <FontAwesomeIcon icon={faEnvelope} className='icon'/>
@@ -41,43 +43,46 @@ const About = () => {
           <FontAwesomeIcon icon={faInstagram} className='social-icon'/>
           <FontAwesomeIcon icon={faLinkedin} className='social-icon'/>
           <FontAwesomeIcon icon={faInstagram} className='social-icon'/>
+          <FontAwesomeIcon icon={faInstagram} className='social-icon'/>
         </div>
       </div>
-    </div>
+      </div>
+
+      <div className='right'>
       <h1>About Me</h1>
       <div className='navbar-setting'><Navbar/></div>
       <hr />
-      <div className='About-context'></div>
       <p className='context-one'>
-        Hey there! 👋 I'm a <b>Software Developer and Freelancer</b> hailing from India, ready to ignite the tech scene! 
-        From idea conception to successful launch, I'm your go-to guy! I thrive on the adrenaline rush of <b>coding competitions and hackathons</b>, 
-        constantly pushing boundaries and thinking outside the box.
+      Hey there! 👋 I'm <b>Prithwi Singh</b>, a dynamic <b>Software Developer and Freelancer </b>from India, ready to set the tech world on fire! From the spark of an idea to a full-fledged launch, I'm your tech virtuoso, transforming concepts into digital masterpieces. I live for the adrenaline rush of coding competitions , where I push the limits and think outside the box.
       </p>
       <p className='context-two'>
-        My passion? <b>Designing and building everything from sleek websites to fully-functional products</b>. But wait, there's more to me!
-        I'm not just about creating – I'm also dedicated to giving back. You'll find me contributing to open-source projects like 
-        <b>#GSoC, SWOC, ZS, Summer of Bitcoin, MLH</b> and more.
+        My passion? <b>Designing and building everything from sleek websites to fully-functional products</b>.
+        As a freelancer, I infuse creativity, precision, and dedication into every project, ensuring exceptional results and delighted clients. Let's join forces and create something extraordinary together! 🚀
       </p>
 
       <h2>Here's What I Do!</h2>
-      <div className="services">
+      <div className="services"> 
         <div className="service">
           <div className='image'>
             <img src={webdev} alt="" />
           </div>
           <div className='service-context'>
           <h3>Web Development</h3>
-          <p>I create stunning websites that look great and work perfectly.</p>
           </div>
-          
         </div>
+
+
+
+
+
+
+
         <div className="service">
           <div className='image'>
             <img src={product} alt="" />
           </div>
           <div className="service-context">
             <h3>Full Stack Products</h3>
-            <p>I build everything you need for your website or app, from the front to the back.</p>
           </div>
         </div>
 
@@ -88,10 +93,8 @@ const About = () => {
           </div>
           <div className='service-context'>
            <h3>Blockchain</h3>
-           <p>I use blockchain to make digital transactions secure and transparent.</p>
           </div>
           
-
         </div>
         <div className="service">
           <div className='image'>
@@ -99,41 +102,14 @@ const About = () => {
           </div>
           <div className="service-context">
             <h3>Artificial Intelligence</h3>
-            <p>I leverage artificial intelligence to make software smart so they can help you work better and faster.</p>
           </div>
-          
         </div>
       </div>
-
-      <h2>"Client Feedback: Their Experiences!"</h2>
-      <div className="testimonials">
-        <div className="testimonial">
-          <div className='image-name'>
-            <img src={product} alt="" />
-            <p>Prithw singh - codingQuery</p>
-          </div>
-          <p>rom idea conception to successful launch, I'm your go-to guy! I thrive on the adrenaline rush of coding competitions and hackathons....</p>
-        </div>
-
-        <div className="testimonial">
-          <div className='image-name'>
-            <img src={product} alt="" />
-            <p>Prithw singh - codingQuery</p>
-          </div>
-          <p>rom idea conception to successful launch, I'm your go-to guy! I thrive on the adrenaline rush of coding competitions and hackathons....</p>
-        </div>
-
-        <div className="testimonial">
-          <div className='image-name'>
-            <img src={product} alt="" />
-            <p>Prithw singh - codingQuery</p>
-          </div>
-          <p>rom idea conception to successful launch, I'm your go-to guy! I thrive on the adrenaline rush of coding competitions and hackathons....</p>
-        </div>
+        
       </div>
+      
   </div>
   );
 };
-
 export default About;
 
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
+  const [menu,setMenu] = useState('about')
   return (
     <nav className="navbar">
       <ul>
-        <li><Link to="/">About</Link></li>
-        <li><Link to="/resume">Resume</Link></li>
-        <li><Link to="/portfolio">Portfolio</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
+      <li onClick={() => setMenu('about')} className={menu === 'about' ? 'active' : ''}>
+          <Link to="/">About</Link>
+        </li>
+        <li onClick={() => setMenu('resume')} className={menu === 'resume' ? 'active' : ''}>
+          <Link to="/resume">Resume</Link>
+        </li>
+        <li onClick={() => setMenu('portfolio')} className={menu === 'portfolio' ? 'active' : ''}>
+          <Link to="/portfolio">Portfolio</Link>
+        </li>
+        <li onClick={() => setMenu('contact')} className={menu === 'contact' ? 'active' : ''}>
+          <Link to="/contact">Contact</Link>
+        </li>
       </ul>
     </nav>
   );
